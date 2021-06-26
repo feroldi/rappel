154c7941cb4aa030b7a9e202da08e1818247aede
Mário Feroldi <mferoldif@gmail.com>
2021-06-26 18:57:29 -0300

    Adding high parts should panic when overflowed

--- rust/arbitrary-precision-integer/src/bigint.rs
+++ rust/arbitrary-precision-integer/src/bigint.rs
@@ -32,55 +32,61 @@ impl Add for BigInt {
 mod tests {
     use super::BigInt;
 
+    const MAX: u64 = u64::MAX;
+
     #[test]
     fn keep_result_in_low_part_when_addition_fits_64_bit() {
         let a = BigInt {
             high: 0,
-            low: u64::MAX - 1,
+            low: MAX - 1,
         };
-        let b = BigInt { high: 0, low: 1u64 };
+        let b = BigInt { high: 0, low: 1 };
 
         let res = a + b;
 
-        assert_eq!(
-            res,
-            BigInt {
-                high: 0u64,
-                low: u64::MAX,
-            }
-        );
+        assert_eq!(res, BigInt { high: 0, low: MAX });
     }
 
     #[test]
     fn carry_out_to_high_when_low_overflows_in_addition() {
-        let a = BigInt {
-            high: 0,
-            low: u64::MAX,
-        };
-        let b = BigInt { high: 0, low: 1u64 };
+        let a = BigInt { high: 3, low: MAX };
+        let b = BigInt { high: 0, low: 1 };
 
         let res = a + b;
 
-        assert_eq!(
-            res,
-            BigInt {
-                high: 1u64,
-                low: 0u64
-            }
-        );
+        assert_eq!(res, BigInt { high: 4, low: 0 });
+    }
+
+    #[test]
+    fn wrap_around_low_addition_when_overflowed() {
+        let a = BigInt {
+            high: 0,
+            low: MAX - 3,
+        };
+        let b = BigInt { high: 0, low: 42 };
+
+        let res = a + b;
+
+        assert_eq!(res, BigInt { high: 1, low: 38 });
     }
 
     #[test]
     fn carrying_out_is_commutative_in_addition() {
-        let a = BigInt {
-            high: 0,
-            low: u64::MAX,
-        };
-        let b = BigInt { high: 0, low: 1u64 };
+        let a = BigInt { high: 0, low: MAX };
+        let b = BigInt { high: 0, low: 1 };
 
         let a_plus_b = a + b;
         let b_plus_a = b + a;
 
         assert_eq!(a_plus_b, b_plus_a);
+    }
+
+    #[test]
+    #[should_panic(expected = "attempt to add with overflow")]
+    fn panic_when_adding_high_parts_overflows() {
+        let a = BigInt { high: MAX, low: 0 };
+        let b = BigInt { high: 1, low: 0 };
+
+        let _ = a + b;
     }
 }
